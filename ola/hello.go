31d8f03cb52fec0c982d4ca2180411e83c0a3751
Wesley Matos <wesley.matos.ext@dasa.com.br>
2021-08-15 21:49:28 -0300

    feat: fill name with 'world' for empty string

--- ola/hello.go
+++ ola/hello.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return "Hello, " + name + "!"
 }
 
--- ola/hello_test.go
+++ ola/hello_test.go
@@ -4,10 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	name := "Wes"
-	result := Hello("Wes")
-	expected := "Hello, " + name + "!"
 
-	if result != expected {
-		t.Errorf("result '%s' | expected '%s'", result, expected)
+	verifyMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+
+		if result != expected {
+			t.Errorf("result '%s' | expected '%s'", result, expected)
+		}
 	}
+
+	t.Run("Should say 'Hello, Wes!'", func(t *testing.T) {
+		result := Hello("Wes")
+		expected := "Hello, " + name + "!"
+
+		verifyMessage(t, result, expected)
+	})
+
+	t.Run("Should say 'Hello, world!' for empty string", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, world!"
+
+		verifyMessage(t, result, expected)
+	})
 }
